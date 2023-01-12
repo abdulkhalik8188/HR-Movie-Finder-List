970cb939bee60d570bd47d1b8da4b142af9be921
kevinnadar <jesikamaraj@gmail.com>
2023-01-13 00:15:36 +0530

    ..

--- translation.py
+++ translation.py
@@ -1,4 +1,4 @@
-START_MESSAGE = "**Hi {}\n\nI'm a bot that search for movies in the Url format. Simply send me the title of a film and I'll send it to you if it's in my database Get Own Bot @Pay_For_Bots_trusted.**"
+START_MESSAGE = "**Hi {}\n\nI'm a bot that search for movies in the Url format. Simply send me the title of a film and I'll send it to you if it's in my database"
 
 NO_RESULTS_FOUND = '''**No Results Found for {}❗️ \n\n 1.Please Write Correct spelling From Google \n\n 2.Do Not add Season or Episode \n\n 3.Do Not add languages or Year \n\n 4.Write only correct movie/Series Name \n\n 5.if Movie Not found Then Request to Admin
 
